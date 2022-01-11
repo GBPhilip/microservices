11279d0c161bb3b607a9c207533f252ce9752994
Philip Sutton <philip.sutton@routeco.com>
2022-01-11 17:11:53 +0000

    Add products

--- ECommerce.Api.Products/Startup.cs
+++ ECommerce.Api.Products/Startup.cs
@@ -1,16 +1,14 @@
+using ECommerce.Api.Products.Db;
+using ECommerce.Api.Products.Interfaces;
+using ECommerce.Api.Products.Providers;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Ecommerce.Api.Products
 {
@@ -26,7 +24,12 @@ namespace Ecommerce.Api.Products
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IProductsProvider, ProductsProvider>();
+            services.AddAutoMapper(typeof(Startup));
+            services.AddDbContext<ProductsDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("Products");
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
